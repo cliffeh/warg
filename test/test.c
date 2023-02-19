0b757a25f903891216901d51a57aead82e8c48a8
Cliff Snyder <cliff.snyder@gmail.com>
2023-02-18 21:20:39 -0500

    added a --verbose arg

--- test/test.c
+++ test/test.c
@@ -9,14 +9,17 @@ main (int argc, const char *argv[])
 
   warg_context option_context;
   warg_opt option_table[]
-      = { { "indent", 'I', "?NUM", "2",
+      = { /* longopt, shortopt, argname, defaultarg, description */
+          { "indent", 'I', "?NUM", "2",
             "indent NUM spaces; implies pretty-printing" },
           { "infile", 'i', "FILE", "-", "read input from FILE" },
           { "outfile", 'o', "FILE", "-", "write output to FILE" },
           { "this-is-a-really-absurdly-unnecessarily-long-flag-name", 'T', 0,
             0, "set all of the doinklets to max frobulation" },
+          { "verbose", 'v', 0, 0, "turn on verbose logging" },
           WARG_AUTOHELP,
-          0 };
+          0
+        };
 
   warg_context_init (&option_context, option_table, argc, argv);
 
@@ -26,7 +29,8 @@ main (int argc, const char *argv[])
         {
         case WARG_UNKNOWN_OPTION:
           {
-            fprintf (stderr, "error: unknown option: %s\n", warg_current_option(&option_context));
+            fprintf (stderr, "error: unknown option: %s\n",
+                     warg_current_option (&option_context));
             warg_print_help (stderr, &option_context);
             exit (1);
           }
